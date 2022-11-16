32f8385aa489c0b23f1f21d8d4c1ddaedc28061d
andreasHovaldt <andreas.hovaldt@gmail.com>
2022-11-16 10:02:12 +0100

    git shit on

--- rgb_and_depth_videostream.py
+++ rgb_and_depth_videostream.py
@@ -9,6 +9,19 @@ frames = {}
 
 downscale_val = (960, 540)
 
+#----- Thresholding -----#
+def get_hsv_data(img):
+    hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    return [np.average(hsv_img[:,:,0]),np.average(hsv_img[:,:,1]),np.average(hsv_img[:,:,2])]
+
+glove_img = cv2.imread("cameraTest_renamed/Kinect V2/glove_colour.jpg")
+glove_hsv = get_hsv_data(glove_img)
+hs = np.array(glove_hsv)*1.2
+ls = np.array(glove_hsv)*0.8
+kernel=np.array([[0,1,1,0],[1,1,1,1],[1,1,1,1],[0,1,1,0]], np.uint8)
+print(kernel.shape)
+
+
 with(device.running()):
     device.start()
     for type_, frame in device:
@@ -16,6 +29,7 @@ with(device.running()):
             
             currentFrame = frame 
             currentFrame = currentFrame.to_array()
+            
         #     currentFrame = np.array(currentFrame, np.uint16)
         #     currentFrame.astype(np.uint16)
         #     currentFrame = currentFrame[:,:,0:3]
@@ -30,7 +44,20 @@ with(device.running()):
                 print(i)
                 #np.savetxt(f'/home/andreas/Desktop/Video_save/txt/{i}.txt',currentFrame)
                 #print(currentFrame.max())
-                cv2.imshow("Video", currentFrame)
+                
+
+                hsv_img = cv2.cvtColor(currentFrame,cv2.COLOR_BGR2HSV)
+
+                mask = cv2.inRange(hsv_img,(ls[0],ls[1],ls[2]),(hs[0],hs[1],hs[2]))
+
+                for i in range(1):
+                    glove = cv2.erode(mask ,kernel)
+                for i in range(3):
+                    glove = cv2.dilate(glove,kernel)
+                
+                
+                cv2.resize(glove,None,fx=0.6,fy=0.6,interpolation=cv2.INTER_LINEAR)
+                cv2.imshow("Video", glove)
             i += 1
         
         
