3bcf8888ec49c3598b103acd089737ca27eaf3a1
ATh0ft <93187726+ATh0ft@users.noreply.github.com>
2022-11-17 12:24:33 +0100

    Update save_as_16bit_png.py

--- Kinect/save_as_16bit_png.py
+++ Kinect/save_as_16bit_png.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import png
 import os.path
-
+import cv2
 
 device = fn2.Device()
 frames = {}
@@ -19,27 +19,29 @@ writer_color = png.Writer(1920,1080, bitdepth=8, greyscale=False)
 i = 0
 images_d = {}
 images_c = {}
-with device.running():
+with(device.running()):
     # For each received frame...
     
     for type_, frame in device:
         # ...stop only when we get an IR frame
-        if type_ is fn2.FrameType.Color:
-            color_frame = frame
-            images_c[i]=np.array((color_frame.to_array), np.uint8)
-            print("color frame")
-
         if type_ is fn2.FrameType.Depth:
             depth_image = frame
-            #depth_image /= depth_image.max()
-            #depth_image = np.sqrt(depth_image)
-            #images_d["D"] = depth_image
             depth_array = frame.to_array()
-            images_d[i]=np.array(depth_array, np.uint16)*7
+            images_d[i]=np.array(depth_array, np.uint16)
+            #print("depth frame")
             #cv2.imshow("IR Image", ir_image)
-        if i >= 10:
-            break
-        i+=1
+        if type_ is fn2.FrameType.Color:
+            color_frame = frame
+            color_array = color_frame.to_array()
+            #print(color_array)
+            images_c[i]=np.array(color_array, np.uint8)
+            #print("color frame")
+
+        
+        
+        if i >= 100:
+            break   
+    i+=1
 #print(images_d[1])
 
 for key in images_d:
@@ -49,10 +51,11 @@ for key in images_d:
         writer_depth.write(f, zgray2list)
 for key in images_c:
     file_location = os.path.join("Kinect/images/color", f"color{key}.png")
-    print(images_c[key])
-    with open(file_location, 'wb') as f:
-        color2list = images_d[key].tolist()
-        writer_color.write(f, color2list)
+    #print(images_c[key])
+    cv2.imwrite(file_location, images_c[key])
+    #with open(file_location, 'wb') as f:
+    #    color2list = images_c[key].tolist()
+    #    writer_color.write(f, color2list)
 
 
 
