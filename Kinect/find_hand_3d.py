bfba91e0c8218c2cef74a79b8d65e296c98680d5
ATh0ft <93187726+ATh0ft@users.noreply.github.com>
2022-11-18 18:58:07 +0100

    Update find_hand_3d.py

--- Kinect/find_hand_3d.py
+++ Kinect/find_hand_3d.py
@@ -21,16 +21,11 @@ def create_depth_img_array(path):
 
 
 
-#add folder to workspace
+#remember to add folder to workspace
 depth_array = create_depth_img_array("/home/alfred/Desktop/hand_track_dataset/test_singlehand/depth")
 
 
 print(f"depth array size {len(depth_array)}")
-
-
-
-
-
 
 
 def conv2_8bit(img16):
@@ -42,7 +37,7 @@ def conv2_8bit(img16):
 def fast_median_noise_reduction(img16,dil_ksize, medfilt_ksize):
     result = np.zeros_like(img16)
     dil_img = cv2.dilate(conv2_8bit(img16), np.ones((dil_ksize,dil_ksize)))
-    cv2.imshow("dil img ", dil_img)
+    #cv2.imshow("dil img ", dil_img)
     mask4 = dil_img > 0
     result[mask4] = scipy.ndimage.median_filter(img16[mask4],medfilt_ksize)
     return result
@@ -73,159 +68,127 @@ def find_human_blob(img16):
     detector = cv2.SimpleBlobDetector_create(params)
 
     rest, img_thresh = cv2.threshold(conv2_8bit(img16), 20,200,cv2.THRESH_BINARY)
-    cv2.imshow("thres", img_thresh)
+    #cv2.imshow("thres", img_thresh)
 
     keypoints = detector.detect(img_thresh)
 
     img_with_keypoints = cv2.drawKeypoints(conv2_8bit(img16), keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
-    cv2.imshow("img with key points", img_with_keypoints)
+    #cv2.imshow("img with key points", img_with_keypoints)
     if len(keypoints) != 0: 
-        print(keypoints[0].pt)
+        #print(keypoints[0].pt)
         return keypoints[0].pt
     else: 
         return False
     
 
 
-
 def find_hand(img16):
-    human_blob = np.zeros_like(img16)
+    #find human blob 
     human_location = find_human_blob(img16)
     if human_location != False:
+        #threshold the image 
         rest, img_thresh = cv2.threshold(conv2_8bit(img16), 20,200,cv2.THRESH_BINARY)
         img_thresh = cv2.erode(img_thresh, np.array([[0,1,1,0],[1,1,1,1],[1,1,1,1],[0,1,1,0]], np.uint8))
+        #detect the blobs 
         labeled_img = scipy.ndimage.label(img_thresh)
 
-        #print(labeled_img[0])
+        #find largest blob 
         largest_blob = 0
-        
         for l in range(np.max(labeled_img[0])):
             bool_arr = labeled_img[0] == l+1
             current_blob_size = np.sum(bool_arr)
             if current_blob_size > largest_blob:
                 largest_blob = current_blob_size
                 largest_blob_lable = l+1
-        #print(f"size {largest_blob}, \t \t lable {largest_blob_lable}")
-
+        
+        #create image where only the largest blob is present 
         mask = labeled_img[0] == largest_blob_lable
-        #print(mask)
         only_largest_blob = np.zeros_like(img16)
-        
         only_largest_blob[mask] = img16[mask]
-        cv2.imshow("only largest blob",conv2_8bit(only_largest_blob))
-        #print(scipy.ndimage.find_objects(labeled_img[0], 1))
-        #print(blob_locations)
+        #cv2.imshow("only largest blob",conv2_8bit(only_largest_blob))
+        
+        #find all the pixel locations of the largest blob 
         blob_locations = np.argwhere(only_largest_blob)
-        #for row in blob_locations:
-        #    print(row)
+        #print(f"blob locations \n {blob_locations}")
 
-        #print(blob_locations)
-        hand_location = blob_locations[np.argmax(blob_locations, axis=0)][0]
-        print(hand_location)
+        #find the pixel with the largest x value 
+        hand_location = blob_locations[blob_locations[:,1].argmax()]
+        #print(f"hand location \n {hand_location}")
         only_largest_blob = conv2_8bit(only_largest_blob)
-        cv2.circle(only_largest_blob,(hand_location[1],hand_location[0]),5,(0,255,0),-1)
-        cv2.imshow("hand", only_largest_blob)
+        final = cv2.cvtColor(only_largest_blob,cv2.COLOR_GRAY2BGR)
+        
+        cv2.imshow("hand", cv2.circle(final,(hand_location[1],hand_location[0]),10,(0,0,255),-1))
+        return hand_location[0:2]
+    else:
+        return False
 
         
         
-        
-            #print(f"col {np.sum(col)}")
+    
 
         
-
-        
-        
-
-        
-
-
-
-
-
-
-
-
 #create static backgound 
 def create_static_backgound(img16):
+    #we copy the imput
     img = img16
-
+    #we crop the image to the size needed 
     img = img[100:400,50:350]
+    #we threshold the depth 
     result = np.zeros_like(img)
 
-
-    mask1 = img > 1200
-    mask2 = img < 2400
+    mask_close = img > 1200
+    mask_far = img < 2400
     
-    mask3 = mask1 == mask2 
-    result[mask3] = img[mask3]
+    mask_total = mask_close == mask_far 
+    result[mask_total] = img[mask_total]
+    #we reduce noice 
     result2 = fast_median_noise_reduction(result,20,(7,7))
     return np.array(result2, np.int16)
 
-
+#we create a static background from the first frame which is empty 
 static_background = create_static_backgound(depth_array[1])
 
 
 prev_time = time.time()
 while True:
     for depth_img in depth_array:
-        #cv2.imshow("color", color_img) 
-        #cv2.waitKey(10)
-        depth_slice_list = [] 
-        
-        #cv2.imshow("start", conv2_8bit(depth_img))
-        #depth_img = scipy.signal.medfilt2d(depth_img,3)
-        
-        
+        #crop the image 
         depth_img = depth_img[100:400,50:350]
 
+        #threshold the depth 
         result = np.zeros_like(depth_img)
         result2 = np.zeros_like(depth_img)
-        mask1 = depth_img > 1200
-        mask2 = depth_img < 2400
+        mask_close = depth_img > 1200
+        mask_far = depth_img < 2400
         
-        mask3 = mask1 == mask2 
-        result[mask3] = depth_img[mask3]
+        mask_total = mask_close == mask_far 
+        result[mask_total] = depth_img[mask_total]
         
-        result2 = fast_median_noise_reduction(result, 20, (7,7))
+        result2 = fast_median_noise_reduction(result, 20, (3,3))
         cv2.imshow("result1",conv2_8bit(result))
-        #dil_img = cv2.dilate(conv2_8bit(result), np.ones((20,20)))
-        #cv2.imshow("dil img ", dil_img)
-        #mask4 = dil_img > 0
-        #result2[mask4] = scipy.ndimage.median_filter(result[mask4],7)
-        #result = scipy.ndimage.median_filter(result,2)
 
-        cv2.imshow("result2",conv2_8bit(result2))
+        #cv2.imshow("result2",conv2_8bit(result2))
 
 
         np_no_bg = np.subtract(np.array(result2, np.int16), static_background) #dont subtract with uint
         np_no_bg = np.array(np.absolute(np_no_bg), np.uint16)
         
-        cv2.imshow("np_no_bg", conv2_8bit(np_no_bg))
+        #cv2.imshow("np_no_bg", conv2_8bit(np_no_bg))
 
         human = np.zeros_like(np_no_bg)
-        #human_filt = np.zeros_like(np_no_bg)
+    
         mask = np_no_bg > 200        
         
         human[mask] = result2[mask]
         
-        cv2.imshow("human",conv2_8bit(human))
+        #cv2.imshow("human",conv2_8bit(human))
 
         
-        human_filt = fast_median_noise_reduction(human,30,(8,8))
-        cv2.imshow("human filt",conv2_8bit(human_filt))
+        human_filt = fast_median_noise_reduction(human,30,(3,3))
+        #cv2.imshow("human filt",conv2_8bit(human_filt))
         find_hand(human_filt)
 
-        #for r in range(10):
-        #    stepsize = (2300-900)/10
-        #    mask1 = depth_img > int(r*stepsize+900)
-        #    mask2 = depth_img < int((r+1)*stepsize+900)
-        #    mask3 = mask1 == mask2 
-        #    result_array = np.zeros_like(depth_img, np.uint16)
-        #    result_array[mask3]=depth_img[mask3]
-        #    depth_slice_list.append(result_array)
-            
-            #cv2.imshow(f"r{r}", conv2_8bit(result_array))
-        cv2.waitKey(10)
-    print(f"time to proces 336 images = {time.time()-prev_time}")
+        cv2.waitKey(1)
+    print(f"time to proces {len(depth_array)} = {time.time()-prev_time} \t avg fps = {len(depth_array)/(time.time()-prev_time)}")
     prev_time = time.time()
             
