8e9f99c565e8e4c2ceb5d1c1fe4cb0613bc09e1e
andreasHovaldt <andreas.hovaldt@gmail.com>
2022-12-19 11:55:14 +0100

    Shell file fix

--- connect_to_ur.sh
+++ connect_to_ur.sh
@@ -2,42 +2,27 @@
 
 # The file will open the relevant node for the UR10
 
-path_to_ws_build=$HOME/UNI/P3/catkin_ws/build/
+path_to_ws_build=$HOME/robot_handover_ros/build/
 
 robot_ip=172.31.1.115
 
 config_path=${path_to_ws_build}../../my_robot_calibration.yaml
 
-gnome-terminal --working-directory=${path_to_ws_build} -- roslaunch ur_robot_driver ur10_bringup.launch robot_ip:=${robot_ip}
-#kinematics_config:=${config_path}
+gnome-terminal --working-directory=${path_to_ws_build} -- roslaunch ur_robot_driver ur10_bringup.launch robot_ip:=${robot_ip} #kinematics_config:=${config_path}
 
 #Uncomment previous line if a config file exists
 
+read -p 'Press "Enter" key when UR-Driver has started'
 
-echo "Press any key when UR-Driver has started"
-while [ true ] ; do
-read -t 5 -n 1
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "Waiting for the keypress"
-fi
-done
 
-gnome-terminal --working-directory=~/ -- roslaunch ur10_moveit_config moveit_planning_execution.launch
+gnome-terminal -- roslaunch ur10_moveit_config moveit_planning_execution.launch
 
 sleep 3
 
-gnome-terminal --working-directory=~/ -- rosrun robot_handover move_ur.py # Ændre den her hvis move_ur.py hedder noget andet
+gnome-terminal -- rosrun robot_handover move_ur.py # Ændre den her hvis move_ur.py hedder noget andet
 
-echo "Press any key to start the camera script"
-while [ true ] ; do
-read -t 5 -n 1
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "Waiting for the keypress"
-fi
-done
+read -p 'Press "Enter" key to start the camera script'
 
 gnome-terminal --working-directory=~/ -- rosrun robot_handover locate_hand_no_background_kinect.py  # Ændre også den her hvis filen hedder noget andet
+
+
