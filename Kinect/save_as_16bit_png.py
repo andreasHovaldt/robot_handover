8c1aca29003c3517febfda73921cd1226b6dfae4
ATh0ft <93187726+ATh0ft@users.noreply.github.com>
2023-01-17 10:23:52 +0100

    more data

--- Kinect/save_as_16bit_png.py
+++ Kinect/save_as_16bit_png.py
@@ -26,13 +26,13 @@ with(device.running()):
     for type_, frame in device:
         # ...stop only when we get an IR frame
         
-        if type_ is fn2.FrameType.Color:
+        if type_ is fn2.FrameType.Color and i % 10:
             color_frame = frame
             color_array = color_frame.to_array()
             #print(color_array)
             images_c[i]=np.array(color_array, np.uint8)
             #print("color frame")
-        if type_ is fn2.FrameType.Depth:
+        if type_ is fn2.FrameType.Depth and i % 10:
             depth_image = frame
             depth_array = frame.to_array()
             images_d[i]=np.array(depth_array, np.uint16)
@@ -40,7 +40,7 @@ with(device.running()):
             #cv2.imshow("IR Image", ir_image)
         
         
-        if i >= 10:
+        if i >= 500:
             break   
         i+=1
 #print(images_d[1])
